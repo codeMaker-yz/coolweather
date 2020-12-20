1d7b13cb2b1a8d3fd8003ffeeb16b7b6e143670a
showdo <582072822@qq.com>
2020-12-20 14:55:50 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/showdo/coolweather/android/MainActivity.java
+++ app/src/main/java/com/showdo/coolweather/android/MainActivity.java
@@ -3,6 +3,18 @@ package com.showdo.coolweather.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.showdo.coolweather.android.util.HttpUtil;
+import com.showdo.coolweather.android.util.Utility;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/test/java/com/showdo/coolweather/android/ExampleUnitTest.java
+++ app/src/test/java/com/showdo/coolweather/android/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.showdo.coolweather.android;
 
+import com.showdo.coolweather.android.util.Utility;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,7 +13,8 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void addition_isCorrect(){
+
     }
+
 }
